6f34c4bce27b19725954795c04cf0a65d6f703fb
Weyler Antonio Uicab Pat <uicabpatweyler@gmail.com>
2019-06-09 09:36:27 -0500

    Actualizacion de Request del Grado escolar
    Eliminacion de codigo que no se usa

--- app/Http/Requests/GradoRequest.php
+++ app/Http/Requests/GradoRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-use phpDocumentor\Reflection\Types\Null_;
 
 class GradoRequest extends FormRequest
 {
