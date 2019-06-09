8cfc3ff4b27da42f78ec5c2cf307f86d4b96d1d4
Weyler Antonio Uicab Pat <uicabpatweyler@gmail.com>
2019-06-09 01:25:59 -0500

    Actualizacion del seeder de los grados
    Se elimina el valor de la columna ciclo, ya que este no forma parte de la tabla grados.

--- database/seeds/GradosTableSeeder.php
+++ database/seeds/GradosTableSeeder.php
@@ -13,71 +13,58 @@ class GradosTableSeeder extends Seeder
     public function run()
     {
         $escuela1=1;
-        $ciclo1=19;
-
         $escuela2=2;
-        $ciclo2=22;
-
 
         Grado::create([
             'escuela_id'  => $escuela1,
-            'ciclo_id'    => $ciclo1,
             'nombre'      => 'Principiante',
             'abreviacion' => 'P'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela1,
-            'ciclo_id'    => $ciclo1,
             'nombre'      => 'Intermedio',
             'abreviacion' => 'I'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela1,
-            'ciclo_id'    => $ciclo1,
             'nombre'      => 'Avanzado',
             'abreviacion' => 'A'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela2,
-            'ciclo_id'    => $ciclo2,
             'nombre'      => 'Primero',
             'abreviacion' => '1ero.'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela2,
-            'ciclo_id'    => $ciclo2,
             'nombre'      => 'Segundo',
             'abreviacion' => '2do.'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela2,
-            'ciclo_id'    => $ciclo2,
             'nombre'      => 'Tercero',
             'abreviacion' => '3ero.'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela2,
-            'ciclo_id'    => $ciclo2,
             'nombre'      => 'Cuarto',
             'abreviacion' => '4to.'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela2,
-            'ciclo_id'    => $ciclo2,
             'nombre'      => 'Quinto',
             'abreviacion' => '5to.'
         ]);
 
         Grado::create([
             'escuela_id'  => $escuela2,
-            'ciclo_id'    => $ciclo2,
             'nombre'      => 'Sexto',
             'abreviacion' => '6to.'
         ]);
