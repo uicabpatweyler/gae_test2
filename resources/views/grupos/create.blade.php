212004d589183a2b52465c1325d680501149f3d2
Weyler Antonio Uicab Pat <uicabpatweyler@gmail.com>
2019-06-10 17:50:21 -0500

    Creacion de las vistas index y create

--- resources/views/grupos/create.blade.php
+++ resources/views/grupos/create.blade.php
@@ -13,7 +13,7 @@
       <h5 class="mb-0 lh-100 text-uppercase">
         <i class="fas fa-plus-circle text-info"></i> nuevo grupo escolar
       </h5>
-      <a href="" class="btn btn-sm blue600 text-white text-uppercase"  role="button" aria-pressed="true" >
+      <a href="{{route('grupos.index')}}" class="btn btn-sm blue600 text-white text-uppercase"  role="button" aria-pressed="true" >
         <i class="far fa-arrow-alt-circle-left"></i> regresar
       </a>
     </div>
@@ -96,113 +96,11 @@
 @push('scripts')
   <!-- Archivo(s) javascript del modulo -->
   <script src="{{ asset('jqueryvalidate-1.19.0/jquery.validate.js') }}"></script>
+  <script src="{{ asset('modulos/grupos.js') }}"></script>
   <script>
     $('#btn_cancelar').click(function(){
       event.preventDefault();
-      showCancel('{{ route('grados.index') }}')
-    });
-
-    $().ready(function() {
-      $('#form_grupo').validate({
-        debug: false,
-        errorElement: "div",
-        rules: {
-          escuela_id: "required",
-          ciclo_id: "required",
-          grado_id: "required",
-          nombre: "required",
-          cupoalumnos: {required: true, min: 1, max: 50}
-        },
-        messages: {
-          escuela_id: "Elija la escuela",
-          ciclo_id: "Elija el ciclo escolar",
-          grado_id: "Elija el grado",
-          nombre: "Falta el nombre del grupo",
-          cupoalumnos: {
-            required: "Obligatorio",
-            min: "Min. 1",
-            max: "Max. 50"
-          }
-        },
-        invalidHandler: function (event, validator) {
-          // 'this' refers to the form
-          var errors = validator.numberOfInvalids();
-          if (errors) {
-            var message = errors === 1
-              ? 'Verifica el campo marcado en rojo'
-              : 'Verifica los ' + errors + ' campos marcados en rojo';
-            showAlert('error', 'ERROR', message, '');
-          } else {
-            // informar que se procedera a guardar el formulario
-          }
-        },
-        submitHandler: function() { saveUpdate(); },
-        errorPlacement: function (error, element) {
-          error.addClass("invalid-feedback");
-          if (element.prop("type") === "checkbox") {
-            error.insertAfter(element.parent("label"));
-          } else {
-            error.insertAfter(element);
-          }
-        },
-        success: function (element) {
-          $(element).remove();
-        },
-        highlight: function (element, errorClass, validClass) {
-          $(element).addClass("is-invalid").removeClass("is-valid");
-        },
-        unhighlight: function (element, errorClass, validClass) {
-          $(element).addClass("is-valid").removeClass("is-invalid");
-        }
-      });
-    });
-
-    function saveUpdate(){
-      $("#btn_guardar").prop('disabled', 'disabled');
-      $.ajax({
-        method: "POST",
-        url: $("#form_grupo").attr('action'),
-        data: $("#form_grupo").serialize()
-      })
-        .done(function(data, textStatus, jqXHR){
-          showAlert(textStatus, jqXHR.statusText, data.message, data.location);
-        })
-        .fail(function( jqXHR, textStatus, errorThrown){
-          var errors = Object.keys(jqXHR.responseJSON.errors).length;
-          var message = errors === 1
-            ? 'Verifica el campo marcado en rojo'
-            : 'Verifica los ' + errors + ' campos marcados en rojo';
-
-          showAlert(textStatus, jqXHR.statusText, message, '');
-
-          $.each(jqXHR.responseJSON.errors, function(key,value){
-            $( "#"+key ).addClass( "is-invalid" ).removeClass( "is-valid" );
-            $('<div id="'+key+'-error" class="error invalid-feedback">'+value+'</div>').insertAfter( $( "#"+key ) );
-          });
-          $("#btn_guardar").removeAttr('disabled');
-        });
-    }
-
-    $.fn.populateSelectGrados = function (values) {
-      var options = '';
-      $.each(values, function (key, row) {
-        options += '<option value="' + row.value + '">' + row.text +' ' +row.abrev + '</option>';
-      });
-      $(this).html(options);
-    };
-
-    $("#escuela_id").change( function (){
-      if($(this).val()!==''){
-        $('#ciclo_id').enableControl(false,true);
-        $('#grado_id').enableControl(false,true);
-
-        $.getJSON(urlRoot+'/data/selectgrados/'+$(this).val(), null, function (values) {
-          $('#grado_id').populateSelectGrados(values);
-        });
-      }
-      else{
-        $('#grado_id').enableControl(true,false);
-      }
+      showCancel('{{ route('grupos.index') }}')
     });
   </script>
 @endpush
---
+++ resources/views/grupos/index.blade.php
@@ -0,0 +1,179 @@
+@extends('master')
+
+{{-- Titulo de la sección--}}
+@section('title', 'Grupos Escolares')
+
+{{--Contenido de la seccion--}}
+@section('content')
+
+  <!-- Contenedor de la seccion -->
+  <div class="col-12 col-md-12 mt-2 pb-3 rounded shadow bg-white border">
+
+    <!-- Titulo de la seccion -->
+    <div class="d-flex align-items-center justify-content-between p-2 my-2 rounded shadow-sm border">
+      <h5 class="mb-0 lh-100 text-uppercase">
+        <i class="fas fa-table text-info"></i> grupos escolares
+      </h5>
+      <a href="{{ route('grupos.create') }}" class="btn btn-sm blue600 text-white text-uppercase"  role="button" aria-pressed="true" >
+        <i class="fas fa-plus"></i> nuevo grupo
+      </a>
+      
+    </div>
+    <!-- Titulo de la seccion -->
+
+    <!-- Formulario, Tablas...etc -->
+    <div class="card border-0">
+      <div class="form-row align-items-center">
+        <div class="col-sm-5 my-1">
+          <label class="sr-only" for="escuela_id">Escuela</label>
+          <select id="escuela_id" name="escuela_id" class="form-control" required>
+            @foreach($escuelas as $escuela)
+              @if($loop->first)
+                <option value="" selected>[Elija una escuela]</option>
+              @endif
+              <option value="{{ $escuela->id }}">{{ $escuela->nombre }} ({{$escuela->nivel->nombre}})</option>
+            @endforeach
+          </select>
+        </div>
+        <div class="col-sm-2 my-1">
+          <label class="sr-only" for="escuela_id">Grado</label>
+          <select id="grado_id" name="grado_id" class="form-control" required disabled="">
+            <option value="" selected>[Elegir grado]</option>
+          </select>
+        </div>
+        <div class="col-sm-2 my-1">
+          <label class="sr-only" for="escuela_id">Ciclo</label>
+          <select id="ciclo_id" name="ciclo_id" class="form-control" required disabled="">
+            @foreach($ciclos as $ciclo)
+              @if($loop->first)
+                <option value="" selected>[Elegir ciclo]</option>
+              @endif
+              <option value="{{ $ciclo->id }}">{{ $ciclo->periodo }})</option>
+            @endforeach
+          </select>
+        </div>
+      </div>
+    </div>
+    <div class="border-bottom border-gray pb-2 mb-2">
+    </div>
+
+    <div class="table-responsive col-12">
+      <table class="table table-striped" id="grupos">
+        <thead>
+        <tr>
+          <th scope="col" class="text-center">GRADO</th>
+          <th scope="col" class="text-center">GRUPO</th>
+          <th scope="col" class="text-center">CUPO</th>
+          <th scope="col" class="text-center">ESTADO</th>
+          <th scope="col" class="text-center">ACCIONES</th>
+        </tr>
+        </thead>
+      </table>
+    </div>
+
+    <!-- Formulario, Tablas...etc -->
+
+  </div>
+  <!-- Contenedor de la seccion -->
+
+@endsection
+@push('scripts')
+  <!-- Axios -->
+  <script src="{{asset('axios-0.19.0/axios.js')}}"></script>
+  <!-- Datatables JS -->
+  <script>
+    var escuela = 0;
+    var grado   = 0;
+    var ciclo   = 0;
+
+    $("#escuela_id").change( function (){
+      if($(this).val()!==''){
+        $('#grado_id').enableControl(true,true);
+        $('#ciclo_id').enableControl(false,true);
+
+        escuela = $(this).val();
+        grado = 0;
+
+        $.getJSON(urlRoot+'/data/selectgrados/'+$(this).val(), null, function (values) {
+          $('#grado_id').populateSelectGrados(values);
+        });
+
+        if(escuela!==0 && grado!==0 && ciclo!==0){
+          filtrarGrupos();
+        }
+      }
+      else{
+        $('#grado_id').enableControl(true,false);
+        escuela = 0;
+        grado = 0;
+      }
+    });
+
+    $('#grado_id').change( function() {
+      if($(this).val()!==''){
+        grado = $(this).val();
+        if(escuela!==0 && grado!==0 && ciclo!==0){
+          filtrarGrupos();
+        }
+      }
+      else{
+        grado = 0;
+      }
+
+    });
+
+    $('#ciclo_id').change(function() {
+      ciclo = $(this).val();
+      if($(this).val()!==''){
+        if(escuela!==0 && grado!==0 && ciclo!==0){
+          filtrarGrupos();
+        }
+      }
+      else{
+        ciclo = 0;
+      }
+    });
+
+    $.fn.populateSelectGrados = function (values) {
+      var options = '';
+      $.each(values, function (key, row) {
+        options += '<option value="' + row.value + '">' + row.text +' ' +row.abrev + '</option>';
+      });
+      $(this).html(options);
+    };
+
+    function filtrarGrupos(){
+      $('#grupos').DataTable({
+        processing: true,
+        serverSide: true,
+        ordering: false,
+        searching: false,
+        destroy: true,
+        ajax: urlRoot + '/data/grupos/' + escuela + '/' + grado + '/' + ciclo,
+        language: {
+          url: "{{ asset('datatables-1.10.19/lang/Spanish.json') }}"
+        },
+        columns: [
+          {data: 'grado.nombre', name: 'grado.nombre', className: "text-center"},
+          {data: 'nombre', name: 'nombre', className: "text-center"},
+          {data: 'cupoalumnos', name: 'cupoalumnos', className: "text-center"},
+          {
+            data: null, className: "text-center",
+            render: function (data) {
+              if (data.status === "1") {
+                return '<i class="fas fa-check text-success"></i>'
+              }
+              return '<i class="fas fa-times text-danger"></i>'
+            }
+          },
+          {
+            data: "actions", className: "text-center",
+            render: function (data) {
+              return htmlDecode(data);
+            }
+          }
+        ]
+      });
+    }
+  </script>
+@endpush
