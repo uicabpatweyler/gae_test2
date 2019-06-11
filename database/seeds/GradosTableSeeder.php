6a894509e714dab5ae72381ed4bead750d6b1c4e
Weyler Antonio Uicab Pat <uicabpatweyler@gmail.com>
2019-06-11 17:42:44 -0500

    Actualizacion del seeder

--- database/seeds/GradosTableSeeder.php
+++ database/seeds/GradosTableSeeder.php
@@ -12,81 +12,78 @@ class GradosTableSeeder extends Seeder
      */
     public function run()
     {
-        $escuela1 = 1;
-        $escuela2 = 2;
-        $escuela3 = 3;
 
         Grado::create([
-            'escuela_id'  => $escuela1,
+            'escuela_id'  => 1,
             'nombre'      => 'Principiante',
             'abreviacion' => 'P'
         ]);
 
         Grado::create([
-            'escuela_id'  => $escuela1,
+            'escuela_id'  => 1,
             'nombre'      => 'Intermedio',
             'abreviacion' => 'I'
         ]);
 
         Grado::create([
-            'escuela_id'  => $escuela1,
+            'escuela_id'  => 1,
             'nombre'      => 'Avanzado',
             'abreviacion' => 'A'
         ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela2,
-            'nombre'      => 'Primero',
-            'abreviacion' => '1ero.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela2,
-            'nombre'      => 'Segundo',
-            'abreviacion' => '2do.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela2,
-            'nombre'      => 'Tercero',
-            'abreviacion' => '3ero.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela2,
-            'nombre'      => 'Cuarto',
-            'abreviacion' => '4to.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela2,
-            'nombre'      => 'Quinto',
-            'abreviacion' => '5to.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela2,
-            'nombre'      => 'Sexto',
-            'abreviacion' => '6to.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela3,
-            'nombre'      => 'primer grado',
-            'abreviacion' => '1ero.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela3,
-            'nombre'      => 'segundo grado',
-            'abreviacion' => '2do.'
-        ]);
-
-        Grado::create([
-            'escuela_id'  => $escuela3,
-            'nombre'      => 'tercer grado',
-            'abreviacion' => '3ero..'
-        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela2,
+//            'nombre'      => 'Primero',
+//            'abreviacion' => '1ero.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela2,
+//            'nombre'      => 'Segundo',
+//            'abreviacion' => '2do.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela2,
+//            'nombre'      => 'Tercero',
+//            'abreviacion' => '3ero.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela2,
+//            'nombre'      => 'Cuarto',
+//            'abreviacion' => '4to.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela2,
+//            'nombre'      => 'Quinto',
+//            'abreviacion' => '5to.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela2,
+//            'nombre'      => 'Sexto',
+//            'abreviacion' => '6to.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela3,
+//            'nombre'      => 'primer grado',
+//            'abreviacion' => '1ero.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela3,
+//            'nombre'      => 'segundo grado',
+//            'abreviacion' => '2do.'
+//        ]);
+//
+//        Grado::create([
+//            'escuela_id'  => $escuela3,
+//            'nombre'      => 'tercer grado',
+//            'abreviacion' => '3ero..'
+//        ]);
 
     }
 }
