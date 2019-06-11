94e4ccd136fe68bfe076501329c0b23c59c5f801
Weyler Antonio Uicab Pat <uicabpatweyler@gmail.com>
2019-06-11 17:42:33 -0500

    Actualizacion del seeder

--- database/seeds/EscuelasTableSeeder.php
+++ database/seeds/EscuelasTableSeeder.php
@@ -13,43 +13,55 @@ class EscuelasTableSeeder extends Seeder
     public function run()
     {
         Escuela::create([
+            'cct' => '23PBT0035N',
+            'incorporacion' => 'ICAT17001CT',
+            'nombre' => 'academia de inglés: irlanda',
             'tipo_id' => 4,
             'nivel_id' => 8,
             'servicio_id' => 20,
-            'nombre' => 'academia de inglés: irlanda',
-            'cct' => '23PBT0035N',
             'turno' => 'Vespertino',
-            'sostenimiento' => 'Privado'
+            'sostenimiento' => 'Privado',
+            'email' => 'academiairlanda@hotmail.com',
+            'telefono' => '(983)-837-6466',
+            'calle' => 'Calle Faisán',
+            'exterior' => '147',
+            'entrecalles' => 'Entre Chablé y Retorno 3',
+            'colonia' => 'Payo Obispo II',
+            'codpost' => '77083',
+            'pais' => 'México',
+            'entidad' => 'Quintana Roo',
+            'municipio' => 'Othón P. Blanco',
+            'localidad' => 'Chetumal'
         ]);
-
-        Escuela::create([
-            'tipo_id' => 1,
-            'nivel_id' => 2,
-            'servicio_id' => 5,
-            'nombre' => 'crescencio carrillo y ancona',
-            'cct' => '31PPR0379E',
-            'turno' => 'Matutino',
-            'sostenimiento' => 'Privado'
-        ]);
-
-        Escuela::create([
-            'tipo_id' => 1,
-            'nivel_id' => 3,
-            'servicio_id' => 8,
-            'nombre' => 'crescencio carrillo y ancona',
-            'cct' => '31PES0080Q',
-            'turno' => 'Matutino',
-            'sostenimiento' => 'Privado'
-        ]);
-
-        Escuela::create([
-            'tipo_id' => 3,
-            'nivel_id' => 6,
-            'servicio_id' => 16,
-            'nombre' => 'crescencio carrillo y ancona',
-            'cct' => '31PBH0024V',
-            'turno' => 'Matutino',
-            'sostenimiento' => 'Privado'
-        ]);
+//
+//        Escuela::create([
+//            'tipo_id' => 1,
+//            'nivel_id' => 2,
+//            'servicio_id' => 5,
+//            'nombre' => 'crescencio carrillo y ancona',
+//            'cct' => '31PPR0379E',
+//            'turno' => 'Matutino',
+//            'sostenimiento' => 'Privado'
+//        ]);
+//
+//        Escuela::create([
+//            'tipo_id' => 1,
+//            'nivel_id' => 3,
+//            'servicio_id' => 8,
+//            'nombre' => 'crescencio carrillo y ancona',
+//            'cct' => '31PES0080Q',
+//            'turno' => 'Matutino',
+//            'sostenimiento' => 'Privado'
+//        ]);
+//
+//        Escuela::create([
+//            'tipo_id' => 3,
+//            'nivel_id' => 6,
+//            'servicio_id' => 16,
+//            'nombre' => 'crescencio carrillo y ancona',
+//            'cct' => '31PBH0024V',
+//            'turno' => 'Matutino',
+//            'sostenimiento' => 'Privado'
+//        ]);
     }
 }
