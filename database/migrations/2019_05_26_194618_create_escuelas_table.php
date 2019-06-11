d3579b8fe87f4f961952afaf5a744ac4631e39fc
Weyler Antonio Uicab Pat <uicabpatweyler@gmail.com>
2019-06-11 17:42:18 -0500

    Actualizacion de la migracion

--- database/migrations/2019_05_26_194618_create_escuelas_table.php
+++ database/migrations/2019_05_26_194618_create_escuelas_table.php
@@ -15,21 +15,23 @@ class CreateEscuelasTable extends Migration
     {
         Schema::create('escuelas', function (Blueprint $table) {
             $table->bigIncrements('id'); //1
+            $table->string('cct',10)->unique();  //6
+            $table->string('incorporacion',20)->nullable(); //7
+            $table->string('nombre',120); //5
             $table->unsignedBigInteger('tipo_id'); //2
             $table->unsignedBigInteger('nivel_id'); //3
             $table->unsignedBigInteger('servicio_id'); //4
-            $table->string('nombre',120); //5
-            $table->string('cct',10)->unique();  //6
-            $table->string('incorporacion',20)->nullable(); //7
             $table->string('turno', 60); //8
             $table->string('sostenimiento', 60); //9
+            $table->string('email',60)->nullable();
+            $table->string('telefono',20)->nullable();
             $table->string('calle',120)->nullable(); //10
             $table->string('exterior',60)->nullable(); //11
             $table->string('interior',60)->nullable(); //12
             $table->string('entrecalles',120)->nullable(); //13
             $table->string('colonia', 120)->nullable(); //14
             $table->string('codpost',5)->nullable(); //15
-            $table->string('pais',30)->nullable(); //16
+            $table->string('pais',30)->default('México'); //16
             $table->string('entidad',30)->nullable(); //17-entidad
             $table->string('municipio',60)->nullable(); //18-municipio
             $table->string('localidad',60)->nullable(); //19
