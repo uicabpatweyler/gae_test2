ba672402d041f21d90e3aee89e61bcedbdeb907b
Weyler Antonio Uicab Pat <uicabpatweyler@gmail.com>
2019-08-01 11:23:41 -0500

    Update report pago inscripciones
    Update report pago inscripciones por dia

--- app/Http/Controllers/Reporte/PagosInscripcionPorDia.php
+++ app/Http/Controllers/Reporte/PagosInscripcionPorDia.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Reporte;
 
+use App\Models\Alumno;
+use App\Models\Config\Grado;
 use App\Models\PagoInscripcion;
-use Carbon\Carbon;
+use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -17,18 +19,13 @@ class PagosInscripcionPorDia extends Controller
   }
 
   public function printPDF($fecha){
-
-    $cancelados = 0;
+    $numeroRecibos = 0;
+    $recibosCancelados = 0;
     $totalReporte = 0;
-    $array_detalle=[];
-    $numeroFila = 0;
+    $rows = [];
+    $numLinea = 1;
 
-    $this->pdf->SetMargins('5','5','5');
-    $this->pdf->SetAutoPageBreak(true,10);
-    $this->pdf->AddPage('P','Letter');
-    $this->pdf->AliasNbPages();
-
-    $rows = PagoInscripcion::whereDate('fecha', $fecha)
+    $pagos = PagoInscripcion::whereDate('fecha', $fecha)
       ->join('alumnos', 'pago_inscripciones.alumno_id','=', 'alumnos.id')
       ->join('ciclos', 'pago_inscripciones.ciclo_id','=','ciclos.id')
       ->join('grupos','pago_inscripciones.grupo_id','=','grupos.id')
@@ -39,114 +36,194 @@ class PagosInscripcionPorDia extends Controller
       ->orderBy('folio_recibo', 'asc')
       ->get();
 
-    foreach ($rows as $row) {
-      if($row->pago_cancelado) {
-        $cancelados++;
-        if($row->fecha === $row->fecha_cancelacion){ $totalReporte = $totalReporte + 0; }
-        if($row->fecha_cancelacion > $row->fecha){ $totalReporte = $totalReporte + $row->cantidad_recibida_mxn;}
+    foreach ($pagos as $pago) {
+      $numeroRecibos++;
+      $cancelType = 0;
+      $alumno = utf8_decode($pago->nombre1.' '.$pago->nombre2.' '.$pago->apellido1.' '.$pago->apellido2);
+      if($pago->pago_cancelado){
+        $recibosCancelados++;
+        if($pago->fecha->equalTo($pago->fecha_cancelacion)){
+          $totalReporte = $totalReporte + 0;
+          $cancelType = 1;
+        }
+        else{
+          $totalReporte = $totalReporte + $pago->cantidad_recibida_mxn;
+        }
       }
       else{
-        $totalReporte = $totalReporte + $row->cantidad_recibida_mxn;
+        $totalReporte = $totalReporte + ($pago->cantidad_concepto * $pago->importe_cuota);
+      }
+
+      $rows[] = [
+        'ciclo'     => $pago->periodo,
+        'numero'    => $numLinea,
+        'recibo'    => $pago->serie_recibo.'-'.$pago->folio_recibo,
+        'cancelado' => $pago->pago_cancelado ? 'Si' : '',
+        'fecha_cancel' => $pago->pago_cancelado ? $pago->fecha_cancelacion->format('d-m-Y') : '',
+        'matricula'    => Alumno::find($pago->alumno_id)->matricula,
+        'alumno'    => $alumno,
+        'nivel'     => Grado::find($pago->grado_id)->nombre,
+        'grupo'     => $pago->grupo,
+        'importe'   => $cancelType === 1 ? 0 : $pago->importe_cuota,
+      ];
+      $numLinea++;
+    }
+
+    $data = $this->detailsRowsReport(count($rows));
+    
+    $this->pdf->SetMargins(5,5,5);
+    $this->pdf->SetAutoPageBreak(true,20);
+    $this->pdf->AliasNbPages();
+
+    for($pagina = 0; $pagina < count($data); $pagina++){
+      $this->pdf->AddPage('P','Letter');
+      $this->tituloPaginacion($fecha, $rows[0]['ciclo']);
+      $this->espacio(5);
+      if($data[$pagina]['page'] === 1){
+        $this->resumen($totalReporte,$numeroRecibos,$recibosCancelados);
+        $this->espacio(5);
+      }
+      $this->encabezadoTabla();
+      $_begin = $data[$pagina]['rowBegin'];
+      $_end   = $data[$pagina]['rowEnd'];
+
+      $this->pdf->SetFillColor(230,242,230);
+      $this->pdf->SetFont('Arial', '', 7);
+      for($_x = $_begin-1; $_x < $_end; $_x++){
+        $fill = $_x % 2 ? true : false;
+        $this->pdf->Cell(8,5,$rows[$_x]['numero'],0,0,'C',$fill);
+        $this->pdf->Cell(14,5,$rows[$_x]['recibo'],0,0,'C',$fill);
+        $this->pdf->Cell(14,5,$rows[$_x]['cancelado'],0,0,'C',$fill);
+        $this->pdf->Cell(16,5,$rows[$_x]['fecha_cancel'],0,0,'C',$fill);
+        $this->pdf->Cell(35,5,$rows[$_x]['matricula'],0,0,'C',$fill);
+        $this->pdf->Cell(55,5,$rows[$_x]['alumno'],0,0,'L',$fill);
+        $this->pdf->Cell(14,5,$rows[$_x]['nivel'],0,0,'C',$fill);
+        $this->pdf->Cell(25,5,$rows[$_x]['grupo'],0,0,'C',$fill);
+        $this->pdf->Cell(25,5,'$ '.number_format($rows[$_x]['importe'],2,'.',','),0,1,'R',$fill);
       }
     }
 
-    $this->pdf->SetFont('Arial', 'B', 8);
-    $this->pdf->Cell(44,6,utf8_decode('Reporte de Inscripciones del día:'),'B',0,'L');
-    $this->pdf->SetFont('Arial', '', 8);
-    $this->pdf->Cell(55,6,' '.$fecha,'B',0,'L');
-    $this->pdf->Cell(10,6,'','B',0,'L');
-    $this->pdf->SetFont('Arial', 'B', 8);
-    $this->pdf->Cell(20,6,utf8_decode('Ciclo Escolar:'),'B',0,'L');
-    $this->pdf->SetFont('Arial', '', 8);
-    $this->pdf->Cell(0,6,$rows->count()!=0 ? $rows[0]['periodo'] : '','B',1,'L');
+    $this->pdf->Output();
+    exit;
+  }
 
-    $this->pdf->Cell(0,1,'',0,1,'',false);
+  private function tituloPaginacion($date, $ciclo){
+    $shortDayOfWeek = array('Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado');
+    $nameMonths     = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre',' Diciembre'];
+    $fecha = (new Carbon($date));
+    $_fecha = $shortDayOfWeek[$fecha->dayOfWeek].' '.$fecha->day.', '.$nameMonths[$fecha->month-1].' '.$fecha->year;
 
+    /*Titulo del reporte  y paginación*/
+    $this->pdf->SetFont('Arial', '', 9);
+    $this->pdf->SetTextColor(0);
+    $this->pdf->SetLineWidth(.2);
+    $this->pdf->SetDrawColor(0);
+    $this->pdf->Cell(60,8,utf8_decode('Reporte de pagos de inscripción del día: '),'B',0,'L',false);
+    $this->pdf->SetFont('Arial', 'B', 9);
+    $this->pdf->Cell(50,8,utf8_decode($_fecha),'B',0,'L',false);
+    $this->pdf->SetFont('Arial', '', 9);
+    $this->pdf->Cell(21,8,'Ciclo:','B',0,'C', false);
+    $this->pdf->SetFont('Arial', 'B', 9);
+    $this->pdf->Cell(25,8,$ciclo,'B',0,'L', false);
+    $this->pdf->SetFont('Arial', '', 8);
+    $this->pdf->Cell(50,8,utf8_decode('Página ').$this->pdf->PageNo().' de {nb}','B',1,'R',false);
+    $this->reset();
+    /*Titulo del reporte  y paginación*/
+  }
+
+  private function resumen($total, $recibos, $cancelados){
     $this->pdf->SetFillColor(230,242,230);
-    $this->pdf->Cell(0,2,'',0,1,false);
     $this->pdf->SetFont('Arial', '', 8);
     $this->pdf->Cell(130,5,'',0,0,false);
     $this->pdf->Cell(38,5,'Total del Reporte','TB',0,'L',false);
     $this->pdf->SetFont('Arial', 'B', 8);
-    $this->pdf->Cell(38,5,'$ '.number_format($totalReporte,2,'.',','),'TB',1,'C',false);
+    $this->pdf->Cell(38,5,'$ '.number_format($total,2,'.',','),'TB',1,'C',true);
     $this->pdf->SetFont('Arial', '', 8);
     $this->pdf->Cell(130,5,'',0,0,false);
     $this->pdf->Cell(38,5,utf8_decode('Número de Recibos'),'B',0,'L',false);
     $this->pdf->SetFont('Arial', 'B', 8);
-    $this->pdf->Cell(38,5,$rows->count(),'B',1,'C',false);
+    $this->pdf->Cell(38,5,$recibos,'B',1,'C',true);
     $this->pdf->SetFont('Arial', '', 8);
     $this->pdf->Cell(130,5,'',0,0,false);
     $this->pdf->Cell(38,5,utf8_decode('Recibos Cancelados'),'B',0,'L',false);
     $this->pdf->SetFont('Arial', 'B', 8);
-    $this->pdf->Cell(38,5,$cancelados,'B',1,'C',false);
-    //Espacio para la siguiente linea
-    $this->pdf->Cell(0,2,'',0,1);
+    $this->pdf->Cell(38,5,$cancelados,'B',1,'C',true);
+    $this->reset();
+  }
 
+  private function encabezadoTabla(){
     $this->pdf->SetFont('Arial', 'B', 7);
     $this->pdf->SetTextColor(255,255,255);
     $this->pdf->SetFillColor(57,107,54);
     $this->pdf->SetFont('Arial', 'B', 7);
     $this->pdf->Cell(8,5,'#',0,0,'C',true);
     $this->pdf->Cell(14,5,'Recibo',0,0,'C',true);
-    $this->pdf->Cell(12,5,'Cancel.',0,0,'C',true);
-    $this->pdf->Cell(40,5,'Matricula',0,0,'C',true);
-    $this->pdf->Cell(50,5,'Alumo',0,0,'C',true);
+    $this->pdf->Cell(14,5,'Cancelado',0,0,'C',true);
+    $this->pdf->Cell(16,5,'F. Cancel',0,0,'C',true);
+    $this->pdf->Cell(35,5,'Matricula',0,0,'C',true);
+    $this->pdf->Cell(55,5,'Alumo',0,0,'C',true);
+    $this->pdf->Cell(14,5,'Nivel',0,0,'C',true);
     $this->pdf->Cell(25,5,'Grupo',0,0,'C',true);
-    $this->pdf->Cell(30,5,'Fecha',0,0,'C',true);
-    $this->pdf->Cell(25,5,'Inscricpion',0,1,'C',true);
+    $this->pdf->Cell(25,5,'Importe',0,1,'C',true);
+    $this->reset();
+  }
 
+  private function detailsRowsReport($totalRows){
+    $aux = [];
+    $allRows        = $totalRows;
+    $rowsFirstPage  = 38;
+    $rowsOthersPage = 42;
+    $rowBegin      = 1;
+    $rowEnd         = $rowsFirstPage;
+    $page           = 1;
 
-
-    if($rows->count()!=0){
-      $this->pdf->SetTextColor(0);
-      $this->pdf->SetFont('Arial', '', 7);
-      $this->pdf->SetFillColor(230,242,230);
-      $i=1;
-
-      foreach ($rows as $row) {
-        $alumno = utf8_decode($row->nombre1.' '.$row->nombre2.' '.$row->apellido1.' '.$row->apellido2);
-        if($row->alumno_id<1000){
-          $matricula = "00"."$row->alumno_id - ".(new Carbon($row->at_created))->format('dmY');
-        }else{
-          $matricula = "$row->alumno_id - ".(new Carbon($row->at_created))->format('dmY');
-        }
-
-        if($i%2===0){$fill = true;} else{$fill=false;}
-        $this->pdf->Cell(8,5,$i,0,0,'C',$fill);
-        $this->pdf->Cell(14,5,$row->folio_recibo,0,0,'C',$fill);
-        $this->pdf->Cell(12,5,$row->pago_cancelado ? 'Si' : '',0,0,'C',$fill);
-        $this->pdf->Cell(40,5,$matricula,0,0,'C',$fill);
-        $this->pdf->Cell(50,5,$alumno,0,0,'L',$fill);
-        $this->pdf->Cell(25,5,$row->grupo,0,0,'C',$fill);
-        $this->pdf->Cell(30,5,$row->fecha->format('Y-m-d'),0,0,'C',$fill);
-        $this->pdf->Cell(25,5,'$ '.number_format($row->importe_cuota,2,'.',','),0,1,'C',$fill);
-        $i++;
-      }
+    if($allRows <= $rowsFirstPage){
+      $aux[] = [
+        'page'     => $page,
+        'rowBegin' => $rowBegin,
+        'rowEnd'   => $allRows
+      ];
     }
+    else{
+      $aux[] = [
+        'page'     => $page++,
+        'rowBegin' => $rowBegin,
+        'rowEnd'   => $rowEnd
+      ];
 
+      $rowBegin = $rowEnd + 1;
+      $_rows    = $allRows - $rowsFirstPage;
+
+      while($_rows > $rowsOthersPage){
+        $aux[] = [
+          'page'     => $page++,
+          'rowBegin' => $rowBegin,
+          'rowEnd'   => $rowBegin + ( $rowsOthersPage - 1 )
+        ];
+        $allRows = $_rows;
+        $rowBegin = $rowBegin + ( $rowsOthersPage - 1 );
+        $rowBegin = $rowBegin + 1;
+        $_rows = $allRows - $rowsOthersPage;
+      }
+      $aux[] = [
+        'page'     => $page++,
+        'rowBegin' => $rowBegin,
+        'rowEnd'   => ($rowBegin + $_rows)-1
+      ];
+    }
+    return $aux;
+  }
+
+  private function espacio($alto){
+    $this->pdf->Cell(0,$alto,'',0,1,'',false);
+    $this->reset();
+  }
+
+  private function reset(){
+    $this->pdf->SetFont('Arial', '', 8);
     $this->pdf->SetTextColor(0);
-    $this->pdf->SetX(0);
-    $this->pdf->SetY(250);
-    $this->pdf->Cell(0,0,'',0,1);
-    $this->pdf->Cell(62,1,'','B',0);
-    $this->pdf->Cell(10,1,'',0,0);
-    $this->pdf->Cell(62,1,'','B',0);
-    $this->pdf->Cell(10,1,'',0,0);
-    $this->pdf->Cell(62,1,'','B',1);
-    $this->pdf->SetFont('Arial', 'B', 8);
-    $this->pdf->Cell(62,4,utf8_decode('María del Carmén Kantun Cupul'),0,0,'C');
-    $this->pdf->Cell(10,4,'',0,0);
-    $this->pdf->Cell(62,4,utf8_decode('Gaspar Felipe Sandoval Gómez'),0,0,'C');
-    $this->pdf->Cell(10,4,'',0,0);
-    $this->pdf->Cell(62,4,utf8_decode('Mtra. Flor Patrón'),0,1,'C');
-    $this->pdf->Cell(62,3,'Encargada de Caja',0,0,'C');
-    $this->pdf->Cell(10,3,'',0,0);
-    $this->pdf->Cell(62,3,'Control Escolar',0,0,'C');
-    $this->pdf->Cell(10,3,'',0,0);
-    $this->pdf->Cell(62,3,'Directora',0,1,'C');
-
-    $this->pdf->Output();
-    exit;
-
+    $this->pdf->SetLineWidth(.2);
+    $this->pdf->SetDrawColor(0);
+    $this->pdf->SetFillColor(255,255,255);
   }
 }
